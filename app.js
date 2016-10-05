33a04fecc709e0eed71c2dcf2de06d4608f035e8
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-05 15:21:04 +0200

    Add fin de scope

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 
 // IIFE : Immediately-Invoked Function Expression
 (function(){
+	var philippe;// undefined
 	console.log('Bienvenue.', 'Ca va ?');
 	
 	// Hoisting	
 	var batiment = "Immeuble 01";
+	
 
-	function freiner(){
-		console.log('fonction freiner PRINCIPALE');
-	}
+	
+	console.log('fonction freiner PRINCIPALE');
+	console.log(philippe);// philippe = undefined
+	philippe = "Philippe Maurice";// "Philippe Maurice"
+	console.log('fonction freiner PRINCIPALE');
+	console.log(philippe);;// philippe = "Philippe Maurice"
+
+
+
+
 
 	function rouler(){
 		console.log('fonction rouler');
-		console.log("Batiment: ", batiment);
 
 		var voiture = "vroom vroom";
 		console.log("Voiture: ", voiture);
@@ -22,19 +30,28 @@
 			console.log('fonction bouger');
 		}
 
-		bouger();// Appelle et exécute la fonction bouger
+		function freiner(){
+			console.log('fonction freiner ==> SECONDAIRE');
+		}
 
-		freiner();
+		// bouger();// Appelle et exécute la fonction bouger
+
+		// freiner();
+
+		var batiment = "3";
+		console.log("Batiment 2: ", batiment);
+		console.log("Batiment 3: ", batiment);
+
 		
 		/* Scope rouler:
-			bouger, voiture
+			bouger, voiture, freiner
 			batiment, rouler, freiner
 
 		*/
 	}
 
 	rouler();
-	freiner();
+	// freiner();
 	// Scope : batiment , rouler, freiner
 
 
