0b4487d3a4b9796edd9055769acf572eb1997fc0
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-05 14:59:01 +0200

    Add first lines about scope.

--- app.js
+++ app.js
@@ -2,5 +2,41 @@
 
 // IIFE : Immediately-Invoked Function Expression
 (function(){
-	console.log('Bienvenue.');
+	console.log('Bienvenue.', 'Ca va ?');
+	
+	// Hoisting	
+	var batiment = "Immeuble 01";
+
+	function freiner(){
+		console.log('fonction freiner PRINCIPALE');
+	}
+
+	function rouler(){
+		console.log('fonction rouler');
+		console.log("Batiment: ", batiment);
+
+		var voiture = "vroom vroom";
+		console.log("Voiture: ", voiture);
+
+		function bouger(){
+			console.log('fonction bouger');
+		}
+
+		bouger();// Appelle et exécute la fonction bouger
+
+		freiner();
+		
+		/* Scope rouler:
+			bouger, voiture
+			batiment, rouler, freiner
+
+		*/
+	}
+
+	rouler();
+	freiner();
+	// Scope : batiment , rouler, freiner
+
+
 })();
+
